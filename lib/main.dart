69ce3575869beb79ff03c51da6ba74b081dfacae
Thinh Nguyen <thinhnd20032007@gmail.com>
2024-08-20 14:07:30 +0700

    The music app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+}
+
+//stateful and stateless
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+//show = stateful
